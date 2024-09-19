c0f9b81fdf0502cfe7e3877f0b53a169232fba8f
Mukilan <146123188+MukilanSF@users.noreply.github.com>
2024-09-19 16:16:17 +0530

    Add files via upload
    A small example LWC using iterator loop

---
+++ iteratorLoop.js
@@ -0,0 +1,24 @@
+import { LightningElement } from 'lwc';
+
+export default class IteratorLoop extends LightningElement {
+    employeeList=[
+        {
+        fName:'Mukilan',
+        lName:'Selvadurai',
+        age:29,
+        city:'Dindigul'
+    },
+    {
+        fName:'Rajesh',
+        lName:'Jeevanantham',
+        age:27,
+        city:'Govindapuram'
+    },
+    {
+        fName:'Praveen',
+        lName:'Kumarasamy',
+        age:24,
+        city:'Puliyamarathukottai'
+    }
+]
+}
